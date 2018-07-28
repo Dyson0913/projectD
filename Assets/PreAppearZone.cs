fdab006176ae66c23c1782ed2687c8fc78b09b01
Dyson0913 <hhg4092@gmail.com>
2018-07-28 12:13:20 +0800

    item collider

--- Assets/PreAppearZone.cs
+++ Assets/PreAppearZone.cs
@@ -23,29 +23,50 @@ public class PreAppearZone : MonoBehaviour
 		
 		for (int i = 0; i < 5;i++ )
         {
-			var instance = Instantiate(obstaic[0],new Vector2(10+i*3,(i+1)*1),transform.rotation);
+			var instance = Instantiate(obstaic[0],new Vector2(3+i*10,(i+1)*1),transform.rotation);
+            instance.gameObject.name = "Item_"+i.ToString();
 			instance.transform.parent = worldCenter.transform;
-            itemQueue.Enqueue(instance);
+            itemQueue.Enqueue(instance);     
+            //itemQueue.ToArray()[i].SetActive(false);
         }
-
-		//showZoneItemFlush();
         
+        
+		//showZoneItemFlush();
+        // Rect rect = this.GetComponent<SpriteRenderer>().sprite.rect;
+        // Debug.Log(" h " +rect.height);
+        // Debug.Log(" w " +rect.width);        
+        
+    }
 
+    void OnTriggerEnter2D(Collider2D col)
+    {       
+        var colname =  col.gameObject.name.Substring(0,4);
+
+        if( colname == "Item"){
+            var colidx = int.Parse(col.gameObject.name.Substring(5));
+            itemQueue.ToArray()[colidx].SetActive(true);
+            //Debug.Log(colidx);
+        }
+        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // var coll2d = Physics2D.OverlapBox(new Vector2(referrenceTransform.transform.position.x,referrenceTransform.transform.position.y),new Vector2(1,1),0);
+        // if( coll2d){
+        //     Debug.Log("touch "+ coll2d.gameObject.name);
+        //     //coll2d.gameObject.SetActive(true);
+        // }
     }
 
 	/*更新區物件刷新 */
     void showZoneItemFlush()
     {
 		for( int i =0; i< itemQueue.Count; i++){
-
+            itemQueue.ToArray()[i].SetActive(false);
 		}
-
     }
 
     public void distanceUpdate(float playerX)
